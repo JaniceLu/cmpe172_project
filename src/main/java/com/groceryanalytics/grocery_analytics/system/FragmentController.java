aa013db10ba1c1e298e93e786ebccc11921408ee
Janice Lu <jjlu555@gmail.com>
2020-04-27 19:03:21 -0700

    Added testing for database connection

--- src/main/java/com/groceryanalytics/grocery_analytics/system/FragmentController.java
+++ src/main/java/com/groceryanalytics/grocery_analytics/system/FragmentController.java
@@ -1,11 +1,19 @@
 package com.groceryanalytics.grocery_analytics.system;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.groceryanalytics.grocery_analytics.store.User;
+import com.groceryanalytics.grocery_analytics.store.UserRepository;
 
 @Controller
 public class FragmentController {
 
+    @Autowired
+
+    private UserRepository userRepository;
+    
     @GetMapping("items")
     public String getItems() {
         return "items.html";
@@ -21,4 +29,9 @@ public class FragmentController {
         return "store.html";
     }
 
+    @GetMapping("all")
+    public @ResponseBody Iterable<User> getAllUsers() 
+    {
+        return userRepository.findAll();
+    }
 }
