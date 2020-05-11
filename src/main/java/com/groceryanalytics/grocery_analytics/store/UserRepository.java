b1a422d6aa962c387cd462008ac6bccaa96a2d97
Janice Lu <jjlu555@gmail.com>
2020-05-11 00:01:36 -0700

    Clean up files and add images
    - add images for README
    - removed user and userrepository class

--- src/main/java/com/groceryanalytics/grocery_analytics/store/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.groceryanalytics.grocery_analytics.store;
-
-import org.springframework.data.repository.CrudRepository;
-import com.groceryanalytics.grocery_analytics.store.User;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-
-}
